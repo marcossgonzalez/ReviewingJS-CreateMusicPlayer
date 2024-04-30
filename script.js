1e8b5eb889470be8fd7633ca6dda01a615ac7ed4
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2024-04-30 19:15:14 -0300

    Update script
    update script.js

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ const playSong = (id) => {
     }
     userData.currentSong = song;
     playButton.classList.add("playing");
+    highlightCurrentSong();
     audio.play();
 };
 const pauseSong = () => {
@@ -97,7 +98,35 @@ const pauseSong = () => {
   audio.pause();
 };
 const playNextSong = () =>{
-  if
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  }
+  else{
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id);
+  }
+};
+const playPreviousSong = () =>{
+  if (userData?.currentSong === null) {
+    return
+  }
+  else{
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex -1];
+    playSong(previousSong.id);
+  }
+};
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+  if (songToHighlight) {
+    songToHighlight.setAttribute("aria-current", "true");
+  }
 };
 const renderSongs = array =>{
     const songsHTML = array.map((song) => {
@@ -128,6 +157,8 @@ playButton.addEventListener("click",() => {
 });
 
 pauseButton.addEventListener("click", pauseSong);
+nextButton.addEventListener("click", playNextSong);
+previousButton.addEventListener("click", playPreviousSong);
 
 const sortSongs = () =>{
     userData?.songs.sort((a,b) =>{
